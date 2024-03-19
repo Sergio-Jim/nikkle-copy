d05a7f91cd2a3cb925b5c1b53a8032b21434bb9c
Sergio-Jim <jimmy1sergio2@gmail.com>
2024-03-19 23:51:21 +0200

    added login and registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import { routes } from "./routes";
+import Login from "./components/userAccess/login";
+import Register from "./components/userAccess/register";
 // import './index.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainLayout/>}>
+        <Route path="/" element={<MainLayout />}>
           {routes}
         </Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
