6fbf674522dae0af773365fdde12de547d1e4b1d
Sergio-Jim <jimmy1sergio2@gmail.com>
2024-04-04 00:46:18 +0200

    added chart

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -8,16 +8,30 @@ const DashboardPage: React.FC = () => {
     if (chartRef.current) {
       const options: ApexCharts.ApexOptions = {
         chart: {
-          type: 'line',
+          type: 'area',
+          height: 350,
         },
-        series: [
-          {
-            name: 'sales',
-            data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
-          },
-        ],
+        series: [{
+          name: 'series1',
+          data: [31, 40, 28, 51, 42, 109, 100]
+        }, {
+          name: 'series2',
+          data: [11, 32, 45, 32, 34, 52, 41]
+        }],
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+          type: 'datetime',
+          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+        },
+        tooltip: {
+          x: {
+            format: 'dd/MM/yy HH:mm'
+          },
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
         },
       };
 
